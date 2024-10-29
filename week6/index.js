dda4ce29f6ba24e53324b1585fa64481b6ae8827
Rohan <rohanshikhare410@gmail.com>
2024-10-29 16:40:03 +0530

    week6-assigment-auth

--- week6/index.js
+++ week6/index.js
@@ -1,52 +1,76 @@
-const express = require('express');
-const  jwt = require('jsonwebtoken');
+const express = require("express");
+const jwt = require("jsonwebtoken");
 const app = express();
+const cors = require("cors");
 
+app.use(cors());
 app.use(express.json());
-const JWT_SECRET = 'RohanS';
+
+const JWT_SECRET = "RohanS";
 let users = [];
 
-app.post('/signup', (req,res) => {
-  const username = req.body.username
-  const password = req.body.password
+function auth(req, res, next) {
+  try {
+    const token = req.headers.token;
+    const verifiedToken = jwt.verify(token, JWT_SECRET);
+    if (verifiedToken.username) {
+      req.username = verifiedToken.username;
+      next();
+    } else {
+      res.json({
+        message: "Access denied. Invalid token.",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ message: "Access denied. Invalid token." });
+  }
+}
+
+app.post("/signup", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
 
   users.push({
     username,
-    password
-  })
-
-  res.status(201).json({ message: 'User created successfully' });
+    password,
+  });
+  console.log(username, password);
+  res.status(201).json({ message: "User created successfully" });
 });
 
-app.post('/signin', (req, res) => {
-  const username = req.body.username
-  const password = req.body.password
+app.post("/signin", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
 
-  const token = jwt.sign({
-    username
-  }, JWT_SECRET);
+  const user = users.find(
+    (user) => user.username === username && user.password === password
+  );
+  if (!user) return res.status(401).json({ message: "Invalid credentials" });
 
+  const token = jwt.sign({ username }, JWT_SECRET);
 
-  console.log(username, password)
+  console.log(username, password);
   res.json({
-    message: 'User signed in successfully',
-    token
+    message: "User signed in successfully",
+    token,
   });
-})
+});
 
-app.get('/me', (req, res) => {
-  const token = req.headers.token
-  const verifiedToken = jwt.verify(token,JWT_SECRET);
+app.get("/me", auth, (req, res) => {
+  const user = users.find((user) => user.username === req.username);
+  if (!user)
+    return res.status(401).json({ message: "Access denied. Invalid ." });
 
-  const username = verifiedToken.username;
-  const user = users.find(user => user.username === username)
-  if (!user) return res.status(401).json({ message: 'Access denied. Invalid token.' });
+  res.json(user);
+});
+
+app.post("/logout", auth, (req, res) => {
   
   res.json({
-    message: 'User authenticated successfully',
-    user
+    message: "User logged out successfully",
   });
-})
+});
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-})
+  console.log("Server is running on port 3000");
+});
